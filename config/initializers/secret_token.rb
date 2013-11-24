82abc5a7505d582819ca7e5ecc660195ce53e296
Mainier <michaelmainier@Michaels-MacBook-Pro.local>
2013-11-24 14:23:06 -0500

    Create impchimp application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impchimp::Application.config.secret_key_base = '583c155fdcf4c8f2d9974cc2d199dc6ae1ed7500de3f76f84032deb03037078bfe808c7fa32fd19a1f37c3b6383641400aa0bfbee4ea33b12a131f261d48839d'
